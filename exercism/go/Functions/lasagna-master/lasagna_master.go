babae679707d88d357678b2e0d685a3d02d3b412
Dilyn Corner <dilyn.corner@tutanota.com>
2022-01-03 02:14:15 -0500

    exercism/go/Functions/lasagna_master: Efficiency.
    I should write something up about these new additions:
    
    ```for _, x := range y {...}
    a := make([]T, b)
    ```
    
    Very powerful things, can reduce some verbosity...

--- exercism/go/Functions/lasagna-master/lasagna_master.go
+++ exercism/go/Functions/lasagna-master/lasagna_master.go
@@ -1,42 +1,34 @@
 package lasagna
 
 func PreparationTime(layers []string, minutes int) int {
-    var time int
     if minutes == 0 {
-        time = len(layers) * 2
-    } else {
-        time = len(layers) * minutes
+        minutes = 2
     }
-    return time
+    return len(layers) * minutes
 }
 
 func Quantities(layers []string) (int, float64) {
-    qNoodle := 0
-    qSauce := 0.0
-    for i := 0; i < len(layers); i++ {
-        switch layers[i] {
-        case "noodles":
+    var qNoodle int
+    var qSauce float64
+    for _, ingredient := range layers {
+        if ingredient == "noodles" {
             qNoodle += 50
-        case "sauce":
+        }
+        if ingredient == "sauce" {
             qSauce +=0.2
         }
     }
         return qNoodle, qSauce
 }
 
-// TODO: define the 'AddSecretIngredient()' function
 func AddSecretIngredient(friendsList, myList []string) []string {
-    secretIngredient := friendsList[len(friendsList)-1]
-    newList := append(myList, secretIngredient)
-    return newList
+    return append(myList, friendsList[len(friendsList)-1])
 }
 
 func ScaleRecipe(quantities []float64, portions int) []float64 {
-    var newQuantities []float64
-    var perPortion float64 = float64(portions) / 2.0
-    for i := 0; i < len(quantities); i++ {
-        newPortion := quantities[i] * perPortion
-        newQuantities = append(newQuantities, newPortion)
+    newQuantities := make([]float64, len(quantities))
+    for i, ingredient := range quantities {
+        newQuantities[i] = (ingredient / 2) * float64(portions)
     }
     return newQuantities
 }
