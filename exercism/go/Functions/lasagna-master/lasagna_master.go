4dea932a9c19e97829e9678da92877c5f0f0b815
Dilyn Corner <dilyn.corner@tutanota.com>
2022-01-08 21:53:06 -0500

    exercism/go: lasagna-master saw a modification

--- exercism/go/Functions/lasagna-master/lasagna_master.go
+++ exercism/go/Functions/lasagna-master/lasagna_master.go
@@ -1,34 +1,36 @@
 package lasagna
 
 func PreparationTime(layers []string, minutes int) int {
-    if minutes == 0 {
-        minutes = 2
-    }
-    return len(layers) * minutes
+	if minutes == 0 {
+		minutes = 2
+	}
+	return len(layers) * minutes
 }
 
 func Quantities(layers []string) (int, float64) {
-    var qNoodle int
-    var qSauce float64
-    for _, ingredient := range layers {
-        if ingredient == "noodles" {
-            qNoodle += 50
-        }
-        if ingredient == "sauce" {
-            qSauce +=0.2
-        }
-    }
-        return qNoodle, qSauce
+	var qNoodle int
+	var qSauce float64
+	for _, ingredient := range layers {
+		if ingredient == "noodles" {
+			qNoodle += 50
+		}
+		if ingredient == "sauce" {
+			qSauce += 0.2
+		}
+	}
+	return qNoodle, qSauce
 }
 
-func AddSecretIngredient(friendsList, myList []string) []string {
-    return append(myList, friendsList[len(friendsList)-1])
+func AddSecretIngredient(friendsList, myList []string) {
+	secretIngredient := friendsList[len(friendsList)-1]
+	newList := myList
+	newList[len(newList)-1] = secretIngredient
 }
 
 func ScaleRecipe(quantities []float64, portions int) []float64 {
-    newQuantities := make([]float64, len(quantities))
-    for i, ingredient := range quantities {
-        newQuantities[i] = (ingredient / 2) * float64(portions)
-    }
-    return newQuantities
+	newQuantities := make([]float64, len(quantities))
+	for i, ingredient := range quantities {
+		newQuantities[i] = (ingredient / 2) * float64(portions)
+	}
+	return newQuantities
 }
