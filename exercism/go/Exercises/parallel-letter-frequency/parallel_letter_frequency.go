2a3a783b4818a270fbd309c77dd1482b3f96eab0
Dilyn Corner <dilyn.corner@tutanota.com>
2022-01-10 21:28:17 -0500

    exercism/go: finish concurrency exercise

--- exercism/go/Exercises/parallel-letter-frequency/parallel_letter_frequency.go
+++ exercism/go/Exercises/parallel-letter-frequency/parallel_letter_frequency.go
@@ -4,6 +4,8 @@ It utilizes concurrency to speed this process up substantially.
 */
 package letter
 
+import "sync"
+
 // FreqMap records the frequency of each rune in a given text.
 type FreqMap map[rune]int
 
@@ -20,38 +22,40 @@ func Frequency(s string) FreqMap {
 // ConcurrentFrequency counts the frequency of each rune in the given strings,
 // by making use of concurrency.
 func ConcurrentFrequency(l []string) FreqMap {
-	// In case l is just too dang small
-	switch len(l) {
-	case 0:
-		return FreqMap{}
-	case 1:
-		return Frequency(l[0])
+
+	// Create a buffered channel to alleviate some pressure between our
+	// goroutines and our channel.
+	freqChan := make(chan FreqMap, len(l))
+
+	// Create a WaitGroup, which acts as a way of controlling concurrency.
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(len(l))
+
+	// This anonymous function acts as a way to close our channel once all of
+	// our goroutines are finished.
+	go func() {
+		waitGroup.Wait()
+		close(freqChan)
+	}()
+
+	// Create a number of goroutines equal to the number of strings in l
+	// When the goroutine has filled its channel, it decrements our wait count.
+	for i := 0; i < len(l); i++ {
+		y := l[i]
+		go func(y string) {
+			defer waitGroup.Done()
+			freqChan <- Frequency(y)
+		}(y)
 	}
 
-	// Create a channel with the type FreqMap ^
-	conChannel := make(chan FreqMap)
-
-	// Create a function that submits the argument l to our channel
-	conFunc := func(l []string) {
-		conChannel <- ConcurrentFrequency(l)
+	// Create a blank map to store the results of our goroutines.
+	finalMap := make(FreqMap)
+	// Range over the contents of the channels we have painstakingly filled...
+	for freqMap := range freqChan {
+		// ... and add the contents to our final tally.
+		for k, v := range freqMap {
+			finalMap[k] += v
+		}
 	}
-
-	// Split the string in half so we can act on each half concurrently
-	halfLen := len(l) / 2
-
-	// Call our concurrency function in a goroutine, one for each half
-	go conFunc(l[:halfLen])
-	go conFunc(l[halfLen:])
-
-	// Create a variable which holds whatever is inside our channel
-	// In this case, it's going to hold the frequency map FreqMap
-	msg := <-conChannel
-
-	// Range over the contents of our channel, incrementing appropriately
-	for r, n := range <-conChannel {
-		msg[r] += n
-	}
-	// Ultimately, this function is merely a clone of Frequency(), just
-	// implemented using concurrency instead of just lollygagging.
-	return msg
+	return finalMap
 }
